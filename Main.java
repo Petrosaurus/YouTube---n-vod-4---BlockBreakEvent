dfa491e76ca58230f2e26f85e71fe7f7da037f48
vlcik128 <24920682+vlcik128@users.noreply.github.com>
2018-10-14 21:23:22 +0200

    Uprava vzhladu kodu - Modernizacia /PullRequest
    Odstránené zbytočné medzery medzi riadkami.
    Všetky riadky sú pokope, dostatočne viditeľné bez potreby scrollovania pri ďalších updatoch kódu.

--- Main.java
+++ Main.java
@@ -1,4 +1,4 @@
-package me.Petrosaurus.BlockBreak;
+ppackage me.Petrosaurus.BlockBreak;
 
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
@@ -9,29 +9,20 @@ import org.bukkit.event.block.BlockBreakEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class Main extends JavaPlugin implements Listener {
-	
-	
+    
 	public void onEnable(){
-		
 		getServer().getPluginManager().registerEvents(this, this);
-		
+
 	}
-	
-	
+
 	@EventHandler
 	public void onBlockBreak(BlockBreakEvent e){
-		
 		if (e.getBlock().getType() ==  Material.DIAMOND_ORE){
-			
 			Bukkit.broadcastMessage(ChatColor.GREEN + "Hrac " + ChatColor.RED + e.getPlayer().getName() + ChatColor.GREEN + " vykopal diamanty");
-			
-			
+
 			//ChatColor
 		}
 		
-			
-			
 	}
-	
 
 }
